e951b5b5894768b92342ef9b77fe82cacf3ef671
Ananda Yulizar <anandayulizar@gmail.com>
2019-09-17 21:31:16 +0700

    BacaMatriks dan TulisMatriks

---
+++ src/Driver.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Driver {
+    public static void main(String[] args) {
+        Matriks M = new Matriks();
+        Matriks.BacaMatriks(M);
+        Matriks.TulisMatriks(M);
+    }
+}
--- src/Matriks.java
+++ src/Matriks.java
@@ -5,10 +5,40 @@ public class Matriks {
     private int baris;
     private int kolom;
 
+    public Matriks() {}
     // Constructor
     public Matriks(int m, int n) {
         this.element = new float[m][n];
         this.baris = m;
         this.kolom = n;
     }
+
+    public static void BacaMatriks(Matriks M) {
+        Scanner scan = new Scanner(System.in);
+        M.baris = scan.nextInt();
+        M.kolom = scan.nextInt();
+        M.element = new float[M.baris][M.kolom];
+
+        for (int i = 0; i < M.baris; i++) {
+            for (int j = 0; j < M.kolom; j++) {
+                M.element[i][j] = scan.nextFloat();
+            }
+        }
+
+        scan.close();
+    }
+
+    public static void TulisMatriks(Matriks M) {
+        for (int i = 0; i < M.baris; i++) {
+            for (int j = 0; j < M.kolom; j++) {
+                if (j == (M.kolom -1)) {
+                    System.out.println(M.element[i][j]);
+                } else {
+                    System.out.print(M.element[i][j] + " ");
+                }
+            }
+            
+        }
+    }
+   
 }
