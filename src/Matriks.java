bfa0806d417c83305ace5a63839de56977f94dc1
Ananda Yulizar <anandayulizar@gmail.com>
2019-09-17 20:26:20 +0700

    Matrix constructor

--- src/Matriks.java
+++ src/Matriks.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Matriks {
-    
+    private float[][] element;
+    private int baris;
+    private int kolom;
+
+    // Constructor
+    public Matriks(int m, int n) {
+        this.element = new float[m][n];
+        this.baris = m;
+        this.kolom = n;
+    }
 }
