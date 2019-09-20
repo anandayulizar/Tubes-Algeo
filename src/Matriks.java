5e031ec788730ac45418a9d7e3d9db4d0794f933
Ananda Yulizar <anandayulizar@gmail.com>
2019-09-20 21:26:06 +0700

    SPL Gauss (butuh perbaikan merinci)

--- src/Matriks.java
+++ src/Matriks.java
@@ -53,15 +53,30 @@ public class Matriks {
         return CopyM;
     }
 
+    /********* Determinan Gauss dan SPL Gauss-Jordan */
+
     public float DeterminanGauss() {
         Matriks tempM = this.CopyMatriks();
+
+        for (int j = 0; j < this.kolom; j++) {
+            if (this.element[j][j] == 0) { 
+                int next = 1;
+                while ((j + next) < this.baris) {
+                    if (this.element[j + next][j] != 0) {
+                        tempM.TukarBaris(j+1, j+next+1);
+                        break;
+                    } else {
+                        next += 1;
+                    }
+                }
+            }
+        }
 
         for (int brs = 1; brs < this.baris; brs++) {
             for (int kol = 0; kol < brs; kol++) {
                 float temp = tempM.element[brs][kol];
                 for (int i = 0; i < this.kolom; i++) {
                     tempM.element[brs][i] = tempM.element[brs][i] - (tempM.element[kol][i] * temp / tempM.element[kol][kol]);
-                    Matriks.TulisMatriks(tempM);
                 }
             }
         }
@@ -75,19 +90,112 @@ public class Matriks {
         return det;
     }
 
+    public void kaliMin() {
+        for (int i = 0; i < this.baris; i++) {
+            for (int j = 0; j < this.kolom; j++) {
+                this.element[i][j] *= (-1);
+            }
+        }
+    }
+
+    public void TukarBaris(int a, int b) {
+        a -= 1;
+        b -= 1;
+
+        Matriks temp = new Matriks(1, this.kolom);
+
+        for (int i = 0; i < this.kolom; i++) {
+            temp.element[0][i] = this.element[a][i];
+            this.element[a][i] = this.element[b][i];
+            this.element[b][i] = temp.element[0][i];
+        }
+
+        this.kaliMin();
+    }
+
+    public void setLeadingOne() {
+        int i = 0;
+        int next;
+        int j = 0;
+        boolean AllZero;
+        while (( i < this.baris) && (j < this.kolom)) {
+            AllZero = false;
+            next = 1;
+            if (this.element[i][j] == 0) {
+                
+                while((i + next)< this.baris) {
+                    if (this.element[i + next][j] != 0) {
+                        TukarBaris(i+1, i+next+1);
+                        break;
+                    } else {
+                        next++;
+                    }
+                }
+                
+            }
+            if ((i+next) == this.baris) {
+                AllZero = true;
+            }
+            j++;
+            if (!AllZero) {
+                i++;
+            }
+        }
+    }
+
+    public void divideByLeading1() {
+        for (int i = 0; i < this.baris; i++) {
+            System.out.println("Baris ke " + i);
+            int lead1 = 0;
+            boolean found = false;
+            float temp = 1;
+            while ((lead1 < this.kolom) && (!found)) {
+                if (this.element[i][lead1] != 0) {
+                    found = true;
+                    temp = this.element[i][lead1];
+                } else {
+                    lead1 += 1;
+                }
+            }
+            for (int j = 0; j < this.kolom; j++) {
+                this.element[i][j] /= temp;
+                System.out.println("Pembagian kol " + j);
+                Matriks.TulisMatriks(this);
+            }
+        }
+    }
+
+    /********** Belom Selesai *****************/
     public static void SPLGauss(Matriks M) {
         Matriks tempM = M.CopyMatriks();
+        int k = 1;
+
+        tempM.setLeadingOne();
+        System.out.println("Tahap pertukaran baris");
+        Matriks.TulisMatriks(tempM);
 
         for (int brs = 1; brs < M.baris; brs++) {
             for (int kol = 0; kol < brs; kol++) {
-                float temp = tempM.element[brs][kol];
-                for (int i = 0; i < (M.kolom - 1); i++) {
-                    tempM.element[brs][i] = tempM.element[brs][i] - (tempM.element[kol][i] * temp / tempM.element[kol][kol]);
-                    System.out.println("Tahap")
-                    Matriks.TulisMatriks(tempM);
+                int kol2 = kol;
+                if (tempM.element[kol][kol2] == 0) {
+                    kol2++;
                 }
+                float temp = tempM.element[brs][kol2];
+                for (int i = 0; i < M.kolom; i++) {
+                    tempM.element[brs][i] = tempM.element[brs][i] - (tempM.element[kol][i] * temp / tempM.element[kol][kol2]);                  
+                    
+                    
+                }
+                System.out.println("Pengurangan baris tahap ke-" + k);
+                Matriks.TulisMatriks(tempM);
+                k++; 
             }
         }
+
+        // Membagi tiap baris dengan leading 1
+        tempM.divideByLeading1();
+        System.out.println("Membagi tiap baris dengan leading 1");
+        Matriks.TulisMatriks(tempM);
     }
     
 }
