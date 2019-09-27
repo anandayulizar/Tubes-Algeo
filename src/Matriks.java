0d573d5f62f243349fc4f27e3dd1a4b7de9616a0
Indra F Nugroho <indrafbrngrh@gmail.com>
2019-09-27 08:21:04 +0700

    bbbb

--- src/Matriks.java
+++ src/Matriks.java
@@ -1001,6 +1001,7 @@ public class Matriks {
                                 }
                                 // if (j != M.kolom) {
                                 M.TulisSolusiTunggal(M);
+                                break;
                                 // }
                             }
                     }
